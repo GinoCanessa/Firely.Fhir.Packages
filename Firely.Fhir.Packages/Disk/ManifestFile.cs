e260ce2d3cf53d0f9ba86bdb4ec324fbc8c16507
mmsmits <marten@fire.ly>
2024-09-30 10:00:20 +0200

    improve sanitize name

--- Firely.Fhir.Packages/Disk/ManifestFile.cs
+++ Firely.Fhir.Packages/Disk/ManifestFile.cs
@@ -196,7 +196,8 @@ namespace Firely.Fhir.Packages
             var builder = new StringBuilder();
             foreach (char c in name)
             {
-                if (c >= 'A' && c <= 'z') builder.Append(char.ToLower(c));
+                if (c >= 'A' && c <= 'Z' && c >= 'a' && c <= 'z')
+                    builder.Append(char.ToLower(c));
             }
             return builder.ToString();
         }
